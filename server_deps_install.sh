80dfe6919d3d5f87764d8e33ab4c710a2db47ed1
Dikkeig <117396888+Dikkeig@users.noreply.github.com>
2022-11-03 21:18:47 +0100

    Add files via upload

---
+++ server_deps_install.sh
@@ -0,0 +1,28 @@
+apt update
+apt install gcc -y
+apt install cmake -y
+apt install build-essential -y
+apt-get install zlib1g-dev -y
+apt install python3-pip -y
+apt install sqlite3 -y
+apt install mariadb-server -y
+apt install python3-click -y
+pip install git+https://github.com/lcdr/utils
+pip install bcrypt
+pip install cffi
+pip install colorama
+pip install dominate
+pip install Flask
+pip install Flask-Bootstrap
+pip install Flask-Login
+pip install Flask-SQLAlchemy
+pip install greenlet
+pip install itsdangerous
+pip install Jinja2
+pip install MarkupSafe
+pip install pycparser
+pip install PyMySQL
+pip install six
+pip install SQLAlchemy
+pip install visitor
+pip install Werkzeug
